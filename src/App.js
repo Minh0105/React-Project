66233f38cd8739e9644a9d6b4b4cef274e12b4df
Minh0105 <minhtrn0105@gmail.com>
2022-12-02 05:39:21 +0700

    Day 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
+import "./App.scss";
+import Nav from "./view/Nav";
+import { useState } from "react";
+//template + logic
+//JSX
+//babel
+const App = () => {
+  let [name, setName] = useState("Min"); // let [a,b] = [1,2] => a=1 b=2
+  let [address, setAddress] = useState("");
+  const handleEventClick = () => {
+    setName(address);
+  };
+  const handleOnChangeInput = (event) => {
+    setAddress(event.target.value);
+  };
+  //jsx có thể in ra giá trị trong 1 obj và k thể in cả obj ra html
+  //re-render
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World</p>
+        <p>Hello World {name}</p>
+        <input
+          type="text"
+          value={address}
+          onChange={(event) => handleOnChangeInput(event)}
+        ></input>
+        <button type="button" onClick={() => handleEventClick()}>
+          click me
+        </button>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +42,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
